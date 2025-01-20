d9e811e0f89240c1a1a278e68e9f2a60b2d0f28c
Pavlo Boiko <boiko.impul@gmail.com>
2025-01-20 12:46:53 -0500

    no message

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "filament",
-            targets: ["Filament", "Bindings"]),
+            targets: ["Filament", "Bindings"] + targets.map({ $0 })),
     ],
     targets: ([
         .target(
