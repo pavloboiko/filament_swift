957b334655fd7f94cd886d4f87e64b908cc630a5
Pavlo Boiko <boiko.impul@gmail.com>
2025-01-20 12:59:02 -0500

    no message

--- Package.swift
+++ Package.swift
@@ -17,7 +17,9 @@ let package = Package(
     targets: ([
         .target(
             name: "Filament",
-            dependencies: ["Bindings"],
+            dependencies: [
+                .target(name: "Bindings"),                
+            ],
             path: "Helpers"
         ),
         .target(
